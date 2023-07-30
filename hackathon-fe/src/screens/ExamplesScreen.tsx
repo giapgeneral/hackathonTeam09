29b909d608472d90d17c74942da15e356b0932e7
duynb <duymanh8683@gmail.com>
2023-07-30 12:24:27 +0700

    a

--- hackathon-fe/src/screens/ExamplesScreen.tsx
+++ hackathon-fe/src/screens/ExamplesScreen.tsx
@@ -3,8 +3,6 @@ import { Button, Image, Text } from "react-native";
 import { atom, useRecoilState } from "recoil";
 
 import { Screen } from "../components/Screen";
-import { Section } from "../components/Section";
-import { SignMessageButton } from "../components/SignMessageButton";
 
 const testAtom = atom<"native" | "bright">({
   key: "testAtom",
@@ -20,38 +18,7 @@ export function ExamplesScreens() {
 
   return (
     <Screen>
-      <Section title="Recoil">
-        <Button
-          title={`The Future is ${future}`}
-          color={
-            future === "bright" ? "rgb(228, 208, 10)" : "rgb(33, 150, 243)"
-          }
-          onPress={() => setFuture(future === "bright" ? "native" : "bright")}
-        />
-      </Section>
-      <Section title="Local Image Import">
-        <Image
-          source={require("../../assets/icon.png")}
-          style={{ width: 50, height: 50 }}
-        />
-        <LearnMoreLink url="https://reactnative.dev/docs/images#static-image-resources" />
-      </Section>
-      <Section title="Custom Font">
-        <Text style={{ fontFamily: "Inter_900Black" }}>
-          Inter 900 Black Font
-        </Text>
-        <LearnMoreLink url="https://docs.expo.dev/guides/using-custom-fonts/#using-a-google-font" />
-      </Section>
-      <Section title="Opening a URL">
-        <Button
-          onPress={() => Linking.openURL("https://xnft.gg")}
-          title="Open xNFT.gg"
-        />
-        <LearnMoreLink url="https://docs.expo.dev/versions/latest/sdk/linking/#linkingopenurlurl" />
-      </Section>
-      <Section title="Signing a message">
-        <SignMessageButton />
-      </Section>
+      <div>Setting page</div>
     </Screen>
   );
 }
